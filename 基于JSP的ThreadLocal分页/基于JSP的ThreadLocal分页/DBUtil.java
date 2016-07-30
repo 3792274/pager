9c22fc59235f0b77d57413ba4385563f75df1f34
3792274 <3792274@qq.com>
2016-07-30 18:12:09 +0800

    f

--- 基于JSP的ThreadLocal分页/基于JSP的ThreadLocal分页/DBUtil.java
+++ 基于JSP的ThreadLocal分页/基于JSP的ThreadLocal分页/DBUtil.java
@@ -3,32 +3,47 @@ package 基于JSP的ThreadLocal分页;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
+
+
 public class DBUtil {
+	
+	private static  String user = "root";
+	private static  String password = "12369";
+	private static  String url = "jdbc:mysql:///pager?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+	
+	
+	private static ThreadLocal<Connection> tl=new ThreadLocal<Connection>();
+	
+	private DBUtil(){}
+ 
  
 	public static Connection getConn(){
-
-		String user = "root";
-		String password = "12369";
-		String url = "jdbc:mysql:///pager?useUnicode=true&characterEncoding=utf-8&useSSL=false";
-		
-		Connection conn =null;
+		Connection connection = tl.get();
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(url,user,password);
+			if(connection == null || connection.isClosed()){
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection(url,user,password);
+				tl.set(connection);
+			} 
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		return conn;
+		} 
+      
+		System.out.println("获得connection对象的HashCode: "+connection.hashCode());
+		return connection;
 	}
 	
- 
+	
+	 
 	
 	//关闭资源
 	public static void close(ResultSet rs, Statement st,Connection conn){
 		try {
+			
 			if(rs!=null)
 				rs.close();
 			if(st!=null)
@@ -37,6 +52,8 @@ public class DBUtil {
 				conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			tl.set(null);
 		}
 	}
 	
